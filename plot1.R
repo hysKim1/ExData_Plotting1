d6fdd3734123e66cdb39dd3960c2bdc1b4c10ba6
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2018-08-07 01:25:29 +0900

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#project 3-1 plotting hyskim
+#plot1
+
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates & times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+#histogram
+attach(data)
+hist(Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+
+# Save file as PNG
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
+detach(data)
